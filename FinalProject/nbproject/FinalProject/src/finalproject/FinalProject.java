28c3544055727fec2519ddaf2bf56622d444a223
Arey A.L. <areyalvarezlopezmail@gmail.com>
2025-12-01 21:55:21 +0100

    Added the LT class

---
+++ FinalProject/nbproject/FinalProject/src/finalproject/FinalProject.java
@@ -0,0 +1,13 @@
+package finalproject;
+
+/**
+ * @author arey
+ */
+
+public class FinalProject {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
